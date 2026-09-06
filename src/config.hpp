d1c2bcbcfaa7087b4aac418efebd597de32689ac
PiterDev <pitertapia884@gmail.com>
2026-09-06 19:02:33 -0300

    feat: expand browser configuration

--- src/config.hpp
+++ src/config.hpp
@@ -11,11 +11,24 @@ public:
 
     const std::string& search_engine() const;
     const std::string& download_directory() const;
-    const std::string& theme() const;
+    const std::string& homepage() const;
+
+    bool ask_download_location() const;
+    bool restore_session() const;
+    bool javascript_enabled() const;
+    bool images_enabled() const;
+    bool sound_enabled() const;
+    bool popups_enabled() const;
 
     void set_search_engine(std::string value);
     void set_download_directory(std::string value);
-    void set_theme(std::string value);
+    void set_homepage(std::string value);
+    void set_ask_download_location(bool value);
+    void set_restore_session(bool value);
+    void set_javascript_enabled(bool value);
+    void set_images_enabled(bool value);
+    void set_sound_enabled(bool value);
+    void set_popups_enabled(bool value);
 
 private:
     static std::string path();
@@ -23,5 +36,11 @@ private:
 
     std::string search_engine_;
     std::string download_directory_;
-    std::string theme_;
+    std::string homepage_;
+    bool ask_download_location_ = false;
+    bool restore_session_ = false;
+    bool javascript_enabled_ = true;
+    bool images_enabled_ = true;
+    bool sound_enabled_ = true;
+    bool popups_enabled_ = true;
 };
