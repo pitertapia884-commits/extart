6f0a5deeec7ca1b9ce693c75d444c8ea6f77f743
PiterDev <pitertapia884@gmail.com>
2026-09-06 19:37:21 -0300

    perf: prepare WebViews for tab close

--- src/tab.hpp
+++ src/tab.hpp
@@ -25,6 +25,7 @@ public:
     void set_title(const char* title);
     void set_active(bool active);
     void apply_config();
+    void prepare_for_close();
     static void apply_config_to_all_tabs();
 
     void find_text(const std::string& text);
