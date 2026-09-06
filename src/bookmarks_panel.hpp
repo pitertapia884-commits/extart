5de73e4c9f91765d80d95b9b9f8ab282d17f9e3c
PiterDev <pitertapia884@gmail.com>
2026-09-06 18:51:34 -0300

    fix: align bookmarks panel header with implementation

--- src/bookmarks_panel.hpp
+++ src/bookmarks_panel.hpp
@@ -9,10 +9,13 @@ class Bookmarks;
 class BookmarksPanel {
 public:
     using OnEntryActivated = std::function<void(const std::string& url)>;
-    using OnBookmarkAdded = std::function<void()>;
+    using OnBookmarkAdded = std::function<void(const std::string& url)>;
 
-    BookmarksPanel(Bookmarks* bookmarks);
-    ~BookmarksPanel();
+    explicit BookmarksPanel(Bookmarks* bookmarks);
+    ~BookmarksPanel() = default;
+
+    BookmarksPanel(const BookmarksPanel&) = delete;
+    BookmarksPanel& operator=(const BookmarksPanel&) = delete;
 
     GtkWidget* create_panel();
     void set_current_page(const std::string& url, const std::string& title);
@@ -21,21 +24,21 @@ public:
     void refresh();
 
 private:
-    static void on_row_activated(GtkListBox* box, GtkListBoxRow* row, gpointer user_data);
-    static void on_add_bookmark_clicked(GtkButton* button, gpointer user_data);
+    static void on_add_clicked(GtkButton* button, gpointer user_data);
+    static void on_open_clicked(GtkButton* button, gpointer user_data);
     static void on_remove_clicked(GtkButton* button, gpointer user_data);
 
     void populate_list();
     void update_add_button();
 
-    Bookmarks* bookmarks_;
+    Bookmarks* bookmarks_ = nullptr;
     std::string current_url_;
     std::string current_title_;
 
+    GtkWidget* panel_ = nullptr;
+    GtkWidget* title_label_ = nullptr;
     GtkWidget* list_box_ = nullptr;
-    GtkWidget* empty_label_ = nullptr;
     GtkWidget* add_button_ = nullptr;
-    GtkWidget* current_page_box_ = nullptr;
 
     OnEntryActivated on_entry_activated_;
     OnBookmarkAdded on_bookmark_added_;
