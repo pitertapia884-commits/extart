ee30e974714a4f2889f62e0f3981533655143968
PiterDev <pitertapia884@gmail.com>
2026-09-19 17:31:16 -0300

    refactor: expose WebKit download backend factory

--- src/download_backend.hpp
+++ src/download_backend.hpp
@@ -1,15 +1,18 @@
 #pragma once
 
+#include <memory>
+
 class DownloadManager;
+struct _WebKitNetworkSession;
 
 class DownloadBackend {
 public:
     virtual ~DownloadBackend() = default;
     DownloadBackend(const DownloadBackend&) = delete;
     DownloadBackend& operator=(const DownloadBackend&) = delete;
-
     virtual void attach(DownloadManager& manager) = 0;
-
 protected:
     DownloadBackend() = default;
 };
+
+std::unique_ptr<DownloadBackend> make_webkit_download_backend(_WebKitNetworkSession* session);
